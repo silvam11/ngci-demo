5b0b561afaa910387a771c9aacfd9f9bba451da8
Mauro Silva <mauro.silva@vce.com>
2018-05-10 18:28:16 +0100

    web-gateway and vcsa-manager communicating

--- web-gateway/server.js
+++ web-gateway/server.js
@@ -1,16 +1,62 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser');
+var request = require('request');
 
-// Constants
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
-// App
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello world\n');
+app.get('/status', (req, res) => {
+  console.log("/status called.");
+  res.status(200).send("OK");
 });
+
+app.post('/user', (req, res) => {
+  console.log("/user called.");
+  console.log("/user req.body : " + req.body);
+  
+  if(!req || !req.body)
+  {
+     var errorMsg = "Invalid argument sent";
+     console.log(errorMsg);
+     return res.status(500).send(errorMsg);
+  }
+
+  console.log("calling " + process.env.VCSA_MANAGER);
+  const options = {
+    url: process.env.VCSA_MANAGER,
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json'
+    }
+  };
+
+  request(options, function(err, resDoc, body) {
+    console.log("callback : " + body);
+    if(err)
+    {
+      console.log("ERROR: " + err);
+      return res.send(err);
+    }
+
+    console.log("statusCode : " + resDoc.statusCode);
+    if(resDoc.statusCode != 200)
+    {
+      console.log("ERROR code: " + res.statusCode);
+      return res.status(500).send(resDoc.statusCode);
+    }
+
+    return res.send({"ok" : body});
+  });
+
+});
+
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
